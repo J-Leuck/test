25d9410f04423f1be81efa939e92b739bce2d061
leuck <luckybibok@gmail.com>
2024-09-08 14:22:46 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,16 @@
+list=["bonjour","monsieur","Vous-allez bien?"]
+
+for i in list:
+    print(i)
+
+reponse=input()
+
+if reponse:
+    print("OH")
+
+if reponse=="oui":
+    print("Tant mieux")
+
+if reponse=="non":
+    print("Ce n'est pas simple")
+
